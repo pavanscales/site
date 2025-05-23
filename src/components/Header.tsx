63b3d655785f3014b530395b3d1cf0db3a86336b
pavanscales <pawanpediredla@gmail.com>
2025-05-23 20:27:41 +0530

    refactor: simplify Header component with dynamic nav links and cleaner markup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,33 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
-  return (
-    <header className="flex justify-between items-center py-8 px-4 md:px-0">
-      <div>
-        <Link to="/" className="text-white hover:text-white no-underline font-bold">
-          pawan
-        </Link>
-      </div>
-      <nav>
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="/blog" className="text-white hover:text-white no-underline">
-              blog
-            </Link>
-          </li>
-          <li>/</li>
-          <li>
-            <Link to="/contact" className="text-white hover:text-white no-underline">
-              contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+const links = [
+  { to: '/blog', label: 'blog' },
+  { to: '/contact', label: 'contact' },
+];
+
+const Header = () => (
+  <header className="flex justify-between items-center py-8 px-4 md:px-0">
+    <div>
+      <Link to="/" className="text-white hover:text-white no-underline font-bold">
+        pawan
+      </Link>
+    </div>
+    <nav>
+      <ul className="flex items-center space-x-4 text-white">
+        {links.map(({ to, label }, i) => (
+          <React.Fragment key={to}>
+            {i > 0 && <li>/</li>}
+            <li>
+              <Link to={to} className="hover:text-white no-underline">
+                {label}
+              </Link>
+            </li>
+          </React.Fragment>
+        ))}
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Header;
