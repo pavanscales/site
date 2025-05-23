792def94c9e05ea1a933e73a37a379550e30897a
pavanscales <pawanpediredla@gmail.com>
2025-05-23 20:30:20 +0530

    refactor: simplify Index component with implicit return

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,14 @@
-
 import React from 'react';
 import Header from '../components/Header';
 import Home from '../components/Home';
 
-const Index = () => {
-  return (
-    <div className="min-h-screen bg-black text-white max-w-2xl mx-auto px-4 md:px-0">
-      <Header />
-      <main className="py-8">
-        <Home />
-      </main>
-    </div>
-  );
-};
+const Index = () => (
+  <div className="min-h-screen bg-black text-white max-w-2xl mx-auto px-4 md:px-0">
+    <Header />
+    <main className="py-8">
+      <Home />
+    </main>
+  </div>
+);
 
 export default Index;
